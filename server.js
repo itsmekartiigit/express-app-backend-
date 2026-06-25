48aebece7d72076588baa4b410971f6b2acf47cf
Itsme kartik.git <itsmekartii.dev@gmail.com>
2026-06-25 21:19:31 +0530

    second commit

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
-
 const PORT = 3000;
 
+// Serve static files
+app.use(express.static(path.join(__dirname, "public")));
+
+// Home route
 app.get("/", (req, res) => {
-  res.send("Hello from AWS EC2 🚀");
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 });
